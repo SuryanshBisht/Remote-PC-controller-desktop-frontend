7fff5ea6ae66ce75049106d107d2e2dbd3892b83
Sahajpreet Singh <sp.code2003@gmail.com>
2023-04-15 14:10:42 +0530

    feat: add routing

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
-import Menu from './components/menu.js';
-import Remote from './components/remote.js';
-import Login from './components/login.js';
-import './App.css';
+import Menu from "./components/menu.js";
+import Remote from "./components/remote.js";
+import Login from "./components/login.js";
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
 
-
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <Menu/>
-      <Remote/>
-      {/*<Login/>*/}
-    </div>
+    <>
+      <div className="App">
+        <Menu />
+        <Routes>
+          <Route path="/remote" element={<Remote />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </div>
+    </>
   );
-}
+};
 
 export default App;
